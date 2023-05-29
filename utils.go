a0c56e1b3ee9b345a7f06750a6ddc73ac546f914
marco <mac.uchiha96@gmail.com>
2023-05-29 18:14:12 +0200

    moved

--- utils.go
+++
@@ -1,8 +0,0 @@
-package main
-
-//	isColor(c) returns true if c is a valid color, false otherwise
-//	STUB: dipendente dalla rappresentazione scelta per i colori;
-//				enumerazione, lista di color-names, codifica RGB/CMYK, hex code, interi,...
-func isColor(color string) bool {
-		return true
-}
